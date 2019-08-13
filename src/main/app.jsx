f03f0d3e0954db53bc4bde23829d67afe072fa86
w1ll-dev <w1llmarcio.dev@hotmail.com>
2019-08-13 11:18:35 -0300

    Adding about.jsx and todo.jsx to the about us page and the task page

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+import About from '../about/about'
+import Todo from '../todo/todo'
 
 
 export default props => (
     <div>
-        <h2>teste</h2>
+        <Todo/>
+        <About/>
     </div>
 )
---
+++ src/todo/todo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <dd>
+        <h1>TODO</h1>
+    </dd>
+)
