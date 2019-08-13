51d459cb774501f39094bb5806a3a7f75a194277
w1ll-dev <w1llmarcio.dev@hotmail.com>
2019-08-13 09:50:57 -0300

    Relocation of index.jsx for adequation in webpack.

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,10 +1,10 @@
+import React from 'react'
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
-import React from 'react'
 
 export default props => (
-    <div className='container'>
-        <p>TESTE</p>
+    <div>
+        <h2>teste</h2>
     </div>
 )
