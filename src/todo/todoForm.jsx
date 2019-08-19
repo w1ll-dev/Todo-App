45b67fd32caf78d8f546dfb6db19f14246002c0e
w1ll-dev <w1llmarcio.dev@hotmail.com>
2019-08-19 13:56:34 -0300

    Component If

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
+
 
 export default props => (
    <div role='form' className="todoForm">
@@ -8,9 +10,7 @@ export default props => (
         </Grid>
 
         <Grid cols="12 3 2">  
-            <button className='btn btn-primary'>
-                 <i className='fa fa-plus'></i>
-            </button>
+           <IconButton style='primary' icon='plus' />
         </Grid> 
    </div>
 )
