d62e0a3df80e18b1ade87d5e1cd204693633f5f8
w1ll-dev <w1llmarcio.dev@hotmail.com>
2019-08-21 15:33:47 -0300

    Add and Remove functionalities.

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
+import axios from 'axios'
 
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
-export default props => (
-    <div>
-        <PageHeader name='Tasks' small=' register'/>
-        <TodoForm/>    
-        <TodoList/>    
-    </div>
-)
+const URL = 'http://localhost:3003/api/todos'
+
+export default class Todo extends Component{
+    constructor(props){
+        super(props)
+        this.handleChange = this.handleChange.bind
+        (this)
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
+
+        this.state = {
+            description: '',
+            list: []
+        }
+    }
+    handleChange(e){
+        this.setState({
+            ...this.state,
+            description: e.target.value,
+            })
+
+    }
+    refresh (){
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    }
+    handleAdd(){
+        const description = this.state.description
+            axios.post( URL, { description })
+                .then(resp => this.refresh())
+    }
+    handleRemove(todo){
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
+    render () {
+        return (
+            <div>
+                <PageHeader name='Tasks' small=' register'/>
+                <TodoForm description = {this.state.description} handleChange = {this.handleChange} handleAdd = {this.handleAdd} />    
+                <TodoList list={this.state.list} handleRemove={this.handleRemove}/>    
+            </div>
+        )
+    }
+}
--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -6,11 +6,12 @@ import IconButton from '../template/iconButton'
 export default props => (
    <div role='form' className="todoForm">
        <Grid cols="12 9 10">
-           <input type="text" className='form-control' placeholder='Add new task'></input>
+           <input type="text" className='form-control' 
+            placeholder='Add new task' value = {props.description} onChange = {props.handleChange} ></input>
         </Grid>
 
         <Grid cols="12 3 2">  
-           <IconButton style='primary' icon='plus' />
+           <IconButton style='primary' icon='plus' onClick = {props.handleAdd}/>
         </Grid> 
    </div>
 )
